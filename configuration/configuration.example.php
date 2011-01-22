c453679d1f5dc00007740cf7141a4864ec12bb2b
Christophe Verbinnen <djpate@gmail.com>
2011-01-22 11:34:46 +0100

    no comment :p

--- configuration/configuration.example.php
+++ configuration/configuration.example.php
@@ -5,7 +5,7 @@
 				'pdoHost'=>'localhost',
 				'pdoUser'=>'root',
 				'pdoDb'=>'kinaf',
-				'pdoPass'=>'okichris');
+				'pdoPass'=>'somepass');
 	
 	$def_route = array("controller"=>"home","action"=>"index","matches"=>array());
 			
