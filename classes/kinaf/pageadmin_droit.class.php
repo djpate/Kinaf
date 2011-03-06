3c819d1f4fa14ec0e0b8dba7b98741e562a328b9
Christophe Verbinnen <djpate@gmail.com>
2011-03-06 12:12:44 +0100

    cleanup

--- classes/kinaf/pageadmin_droit.class.php
+++
@@ -1,6 +0,0 @@
-<?php
-	class pageadmin_droit extends modele {
-		protected static $table = "pageadmin_droit";
-		
-	}
-?>
---
+++ configuration/sql/kinaf.sql
@@ -0,0 +1,46 @@
+-- phpMyAdmin SQL Dump
+-- version 3.3.7deb1
+-- http://www.phpmyadmin.net
+--
+-- Serveur: localhost
+-- Généré le : Ven 05 Novembre 2010 à 00:22
+-- Version du serveur: 5.1.49
+-- Version de PHP: 5.3.3-1ubuntu9.1
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de données: `kinaf`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `admin`
+--
+
+CREATE TABLE IF NOT EXISTS `admin` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(50) NOT NULL,
+  `prenom` varchar(50) NOT NULL,
+  `login` varchar(50) NOT NULL,
+  `password` varchar(200) NOT NULL,
+  `Level` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- Contenu de la table `admin`
+--
+
+INSERT INTO `admin` (`id`, `nom`, `prenom`, `login`, `password`, `Level`) VALUES
+(1, 'Verbinnen', 'Christophe', 'djpate', 'ade972a26def473f900b4a5e06093f0d1d2ca028a0e2f151eb8c79aca19b0426c9aecf0292cd8b3521ecd9df45913eb90a888c4ac538a01211f01f63e22c61d7', 1);
+
+-- --------------------------------------------------------
+
