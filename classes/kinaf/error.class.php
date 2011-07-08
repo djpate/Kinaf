95a4ec9e1fd249ddc399eddc6bd294424e8b41ac
Christophe Verbinnen <djpate@gmail.com>
2011-07-08 12:19:57 +0200

    suite

--- classes/kinaf/error.class.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace kinaf;
-	class error {
-		public function __construct($err){
-			echo "<div style=\"border:2px solid red;padding:10px\">$err</div>";
-			error_log($err);
-		}
-	}
-?>
