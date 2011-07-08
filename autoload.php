1f2242cda50d059dba7b3d23e606568a11001df9
Christophe Verbinnen <djpate@gmail.com>
2011-07-08 09:01:55 +0200

    refacto + ajout twig & yaml

--- autoload.php
+++ autoload.php
@@ -1,7 +1,16 @@
 <?
+
+	/* kinaf autoload */
+
     set_include_path(get_include_path().PATH_SEPARATOR.__dir__."/classes".PATH_SEPARATOR.__dir__."/../namespace");
-
     spl_autoload_extensions('.class.php,.php');
-
     spl_autoload_register();
+    
+    /* libs autoload */
+    
+    include_once(__dir__."/libs/yaml/SfYaml.php");
+    include_once(__dir__."/libs/Twig/Autoloader.php");
+    
+    Twig_Autoloader::register();
+    
 ?>
