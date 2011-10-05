99296a2c720e0569a631b7fca4c9fb6c158779a5
korben90 <gaultier.monier@gmail.com>
2011-10-05 11:51:31 +0200

    wrapper mongo

---
+++ classes/kinaf/mongo.class.php
@@ -0,0 +1,62 @@
+<?
+namespace kinaf;
+
+class Mongo extends Singleton {
+	
+	private $mgInstance;
+    public $collection;
+ 
+    protected function __construct() {
+		$conf = Configuration::get();
+		$this->mgInstance = new \Mongo($conf['mongo']['host'].':'.$conf['mongo']['port']);
+        $this->mongo = $this->mgInstance->$conf['mongo']['dbname'];
+	}
+
+    public function collection() {
+    	return $this->collection;
+    }
+
+    public function setCollection($nom){
+		$this->collection = $this->mgInstance->$nom;
+	}
+
+	public function getConnexion(){
+		return $this->mongo->{$this->collection};
+	}
+
+	public function save($params, $options = null) {
+		if(is_object($params)) {
+			$params = $this->cleanData($params, $params->orm->getFields());
+		} else {
+			exit;
+		}
+
+		try {
+			 $this->mongo->{$this->collection}->insert($params, $options);
+			 return $params->_id;
+		}
+		catch(\MongoCursorException $e) {
+			//l'enregistrement existe déjà
+			$this->mongo->{$this->collection}->save($params, $options);
+		}
+		
+	}
+
+	public function cleanData($object, $fields) {
+		$results = new \stdClass();
+		foreach($fields as $key=>$val){
+			$results->$val = $object->$val;
+		}
+		return $results;
+	}
+
+	public function cleanArrayData($array) {
+		$results = array();
+		foreach($array as $o) {
+			array_push($results, $this->cleanData($o, $o->orm->getFields()));
+		}
+		return $results;
+	}
+   
+}
+?>
